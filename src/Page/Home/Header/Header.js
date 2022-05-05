13665faeefaa3564d285d85c403fa1cdfc1b8766
ainul1997 <ainul4147@gmail.com>
2022-05-05 18:10:53 +0600

    added footer commponent, addting header commponent

--- src/Page/Home/Header/Header.js
+++ src/Page/Home/Header/Header.js
@@ -17,20 +17,20 @@ const Header = () => {
                     <a style={{ fontSize: "30px", borderRadius: '50px' }} className="navbar-brand" href="#"> <img src={cargoexpress} alt="" />  </a>
 
                     <div className="collapse navbar-collapse mx-5" id="navbarNav">
-                        <div className='mx-3'>
-                            <p> Call Us Anytim</p>
+                        <div className='mx-3 '>
+                            <p className='d-flex align-items-center ' style={{ color: '#CCCCCC' }}>  <img style={{ height: '20px', marginRight: '5px', marginLeft: '5px' }} src='https://icons-for-free.com/download-icon-phone+line+icon+headphone+headphone+icon+icon-1320073120771382953_512.png' alt="" /> Call Us Anytim</p>
                             <p> 1-888-123-2345</p>
 
                         </div>
                         <div style={{ width: '1px', height: '40px', margin: '5px' }} className='bg-dark'></div>
                         <div className='mx-5'>
-                            <p> Opening Time</p>
+                            <p className='d-flex align-items-center ' style={{ color: '#CCCCCC' }} > <img style={{ height: '20px', marginRight: '5px', marginLeft: '5px', opacity: '30%' }} src='https://www.nicepng.com/png/detail/840-8403859_alarm-clock-svg-png-icon-free-download-alarm.png' alt="" /> Opening Time</p>
                             <p>  08:00 - 18:00</p>
 
                         </div>
                         <div style={{ width: '1px', height: '40px', margin: '5px' }} className='bg-dark'></div>
                         <div className='mx-5'>
-                            <p> Email Us</p>
+                            <p style={{ color: '#CCCCCC' }}>  <img style={{ height: '20px', marginRight: '5px', marginLeft: '5px', opacity: '30%' }} src='https://www.designbust.com/download/1026/png/email_icon_transparent_background512.png' alt="" /> Email Us</p>
                             <p> info@cargoexpress.io</p>
                         </div>
 
--- src/Page/Home/Service/Service.js
+++ src/Page/Home/Service/Service.js
@@ -6,29 +6,27 @@ import React from 'react';
 const Service = ({ service }) => {
     const { name, description, img } = service;
     return (
-        <div>
-            <div className=''>
-                <div >
-                    <div className='content_cont mt-5'>
-                        <div className='content-box p-2 flex d-flex'>
-                            <div><h3 className='fw-bolder fs-6 mt-3'>{name}</h3>
-                                <p className='text-capitalize fw-lighter fs-6'> {description}</p>
-                            </div>
-                            <div>
-                                <img style={{ height: '50px', marginTop: '20px', marginRight: '5px', marginLeft: '5px' }} src={img} alt="" />
-                            </div>
-                        </div>
-
-                        <div className='border-box'></div>
+        <div  >
+            <div className='content_cont mt-5'>
+                <div className='content-box p-2 flex d-flex'>
+                    <div><h3 className='fw-bolder fs-6 mt-3'>{name}</h3>
+                        <p className='text-capitalize fw-lighter fs-6'> {description}</p>
                     </div>
-                    <div className=''>
-
+                    <div>
+                        <img style={{ height: '50px', marginTop: '20px', marginRight: '5px', marginLeft: '5px' }} src={img} alt="" />
                     </div>
                 </div>
 
+                <div className='border-box'></div>
             </div>
+            <div className=''>
 
+            </div>
         </div>
+
+
+
+
     );
 };
 
